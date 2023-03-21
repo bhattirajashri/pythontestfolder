4eccc0ca617a3582e6ab0cb205a25c0ce642cf20
bhattirajashri <128449442+bhattirajashri@users.noreply.github.com>
2023-03-21 10:24:42 +0530

    Add files via upload

---
+++ untitled14.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 16 10:33:45 2023
+
+@author: iant
+"""
+
+import os
+#os.mkdir("testingfolder")
+#os.getcwd()
+#os.chdir("D:")
+#os.getcwd()
+os.rmdir("testingfolder")
+#os.getcwd()
